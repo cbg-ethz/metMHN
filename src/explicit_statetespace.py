eb246f033eaf6f9a6b7c23e3570afa96f381c729
huy29433 <yanren-linda.hu@stud.uni-regensburg.de>
2022-11-28 10:40:26 +0100

    formatting

--- src/explicit_statetespace.py
+++ src/explicit_statetespace.py
@@ -3,24 +3,30 @@ import pandas as pd
 from itertools import compress
 
 # Some helper functions
+
+
 def dia2(tij):
     ret = np.diag([-tij, 0])
     ret[1, 0] = tij
     return ret
 
 # List all possible states in lexicographic order
+
+
 def state_space(n):
     return [f'{i:05b}'[::-1] for i in range(2 ** n)]
 
 # List all possible states, that datapoint could have visited
+
+
 def trunk_states(dpoint):
     n = len(dpoint)
     inds = np.ones(1)
     for i in range(n):
         if dpoint[i] == 1:
-            inds = np.kron(np.array([1,1]), inds)
+            inds = np.kron(np.array([1, 1]), inds)
         else:
-            inds = np.kron(np.array([1,0]), inds)
+            inds = np.kron(np.array([1, 0]), inds)
 
     return [state for state in compress(state_space(n), inds)]
 
@@ -54,25 +60,30 @@ def build_q(theta):
     Q = np.zeros([2**(2*n+1), 2**(2*n+1)])
     for i in range(n):
         sync_off = np.diag([-theta[i, i], 0., 0., 0.])
-        sync_off[3,0] = theta[i, i]
-        sync = multi_kron_prods(diag_sync, sync_off, np.diag([1., 0.]), theta, i, n)
+        sync_off[3, 0] = theta[i, i]
+        sync = multi_kron_prods(diag_sync, sync_off,
+                                np.diag([1., 0.]), theta, i, n)
         prim_off = np.diag([-1., 0., -1., 0.])
         prim_off[1, 0] = 1.
         prim_off[3, 2] = 1.
-        prim = multi_kron_prods(diag_prim, prim_off*theta[i, i], np.diag([0, 1]), theta, i, n)
+        prim = multi_kron_prods(diag_prim, prim_off *
+                                theta[i, i], np.diag([0, 1]), theta, i, n)
         met_off = np.diag([-1., -1., 0., 0.])
         met_off[2, 0] = 1.
         met_off[3, 1] = 1.
-        met = multi_kron_prods(diag_met, met_off*theta[i, i], np.diag([0., theta[i, n]]), theta, i, n)
+        met = multi_kron_prods(
+            diag_met, met_off*theta[i, i], np.diag([0., theta[i, n]]), theta, i, n)
         Q = Q + sync + prim + met
     off_seed = np.diag([-theta[n, n], 0.])
-    off_seed[1,0] = theta[n, n]
+    off_seed[1, 0] = theta[n, n]
     Q = Q + multi_kron_prods(diag_sync, off_seed, 1.0*np.eye(1), theta, n, n)
     return Q
 
 # Methods for explicit calculation of the SSR version of Q
 
 # Builds Q_i for the synchronized part of Q
+
+
 def sync_ssr_q(mut, theta, i, n):
     syncL = np.diag([-1, 0, 0, 0])
     syncL[3, 0] = 1
@@ -101,6 +112,8 @@ def sync_ssr_q(mut, theta, i, n):
     return Q
 
 # Builds Q_i for the metastatic part of Q after seeding
+
+
 def met_ssr_q(mut, theta, i, n):
     metL = np.diag([-1, -1, 0, 0])
     metL[2, 0] = 1
@@ -116,7 +129,8 @@ def met_ssr_q(mut, theta, i, n):
         elif mut[j] == 2:
             Q = np.kron(np.diag([1, theta[i, j]]), Q)  # ...|01|...
         elif mut[j] == 3:
-            Q = np.kron(np.diag([1, 1, theta[i, j], theta[i, j]]), Q)  # ...|11|...
+            # ...|11|...
+            Q = np.kron(np.diag([1, 1, theta[i, j], theta[i, j]]), Q)
     # Non diagonal Kronecker factors
     if mut[i] == 0:
         Q = Q * (-theta[i, i])  # ...|00|...
@@ -138,6 +152,8 @@ def met_ssr_q(mut, theta, i, n):
     return Q
 
 # Builds Q_i for the primary part of Q after seeding
+
+
 def prim_ssr_q(mut, theta, i, n):
     primL = np.diag([-1, 0, -1, 0])
     primL[1, 0] = 1
@@ -173,6 +189,8 @@ def prim_ssr_q(mut, theta, i, n):
     return Q
 
 # Adds the entries of the seeding event
+
+
 def seeding_ssr_q(dpoint, theta, n):
     mut = [dpoint[j] + 2 * dpoint[j + 1] for j in range(0, 2 * n, 2)]
     diag10 = np.diag([1, 0])
@@ -194,5 +212,6 @@ def ssr_build_q(dpoint, theta):
     mut = [dpoint[j] + 2 * dpoint[j + 1] for j in range(0, 2 * n, 2)]
     Q = np.zeros(2 ** (sum(dpoint)))
     for i in range(n):
-        Q = Q + sync_ssr_q(mut, theta, i, n) + met_ssr_q(mut, theta, i, n) + prim_ssr_q(mut, theta, i, n)
+        Q = Q + sync_ssr_q(mut, theta, i, n) + met_ssr_q(mut,
+                                                         theta, i, n) + prim_ssr_q(mut, theta, i, n)
     return Q + seeding_ssr_q(dpoint, theta, n)
