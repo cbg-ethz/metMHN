99f3f0791fb5298941bb97f19fdcecd2dcf11962
huy29433 <yanren-linda.hu@stud.uni-regensburg.de>
2022-12-05 15:38:53 +0100

    added reachable check to fss likelihood

--- src/likelihood.py
+++ src/likelihood.py
@@ -1,5 +1,6 @@
 import kronecker_vector as fss
 import numpy as np
+import Utilityfunctions as utils
 
 
 def jacobi(log_theta: np.array, b: np.array, lam: float, x: np.array = None) -> np.array:
@@ -17,7 +18,7 @@ def jacobi(log_theta: np.array, b: np.array, lam: float, x: np.array = None) -> 
     if x is None:
         x = np.ones(2**(2*n+1))/(2**(2*n+1))
     dg = fss.diag_q(log_theta) + lam
-    
+
     for _ in range(2*n+2):
         x = b + fss.qvec(log_theta, x, diag=False)
         x = x/dg
@@ -35,6 +36,11 @@ def log_likelihood(log_theta: np.array, pd: np.array, lam1: float, lam2: float) 
     Returns:
          float: pd^T pTh
     """
+
+    reachable = utils.reachable_states(log_theta.shape[0] - 1)
+    if np.any(pd[~reachable] != 0):
+        raise ValueError("The data vector contains unreachable states.")
+
     n = log_theta.shape[0] - 1
     p0 = np.zeros(2**(2*n+1))
     p0[0] = 1
@@ -42,4 +48,6 @@ def log_likelihood(log_theta: np.array, pd: np.array, lam1: float, lam2: float) 
     pTh = jacobi(log_theta, p0, lam2)-jacobi(log_theta, p0, lam1)
     pTh = lam1*lam2/(lam1-lam2)*pTh
 
-    return pd.dot(np.nan_to_num(np.log(pTh)))
+    return pd[reachable].dot(
+        np.log(pTh[reachable])
+    )
