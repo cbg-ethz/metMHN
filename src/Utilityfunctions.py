b708f063db9656bebdc532ad71dc2d511fd6c1ba
Yanren Linda Hu <yanren-linda.hu@stud.uni-regensburg.de>
2022-12-05 08:44:02 +0100

    layout

--- src/Utilityfunctions.py
+++ src/Utilityfunctions.py
@@ -15,7 +15,7 @@ def state_space(n: int) -> list:
 def trunk_states(dpoint: list) -> list:
     """
     Args:
-        dpoint (list): Bitsting, genotype of a single patient
+        dpoint (list): Bitstring, genotype of a single patient
     Returns:
         list: states that the tumor could have visited during progression
     """
@@ -57,14 +57,16 @@ def random_theta(n: int, sparsity: float) -> np.array:
         np.array: theta
     """
     npone = n + 1
-    theta = np.zeros((npone, npone))
-    theta += np.diag(np.random.normal(size=npone))
-    index = np.argwhere(theta == 0)[
+    log_theta = np.zeros((npone, npone))
+    log_theta += np.diag(np.random.normal(size=npone))
+    index = np.argwhere(log_theta == 0)[
         np.random.choice(npone**2-npone, size=int((npone**2-npone)
                          * (1-sparsity)), replace=True)
     ]
-    theta[index[:, 0], index[:, 1]] = np.random.normal(
+    log_theta[index[:, 0], index[:, 1]] = np.random.normal(
         size=int((npone**2-npone)*(1-sparsity)))
+    return log_theta
+
 
 def reachable_states(n: int):
 
