8354a285c59086929e8dcc613f04cbb9cbbb531e
Yanren Linda Hu <yanren-linda.hu@stud.uni-regensburg.de>
2022-12-05 08:45:36 +0100

    bugfix

--- src/ssr_likelihood.py
+++ src/ssr_likelihood.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from ssr_kronecker_vector import kronvec_sync, kronvec_met, kronvec_prim, kronvec_seed, kronvec, kron_diag
+import Utilityfunctions as utils
 
 
 def x_partial_Q_y(log_theta: np.array, x: np.array, y: np.array, state: np.array) -> np.array:
@@ -18,9 +19,9 @@ def x_partial_Q_y(log_theta: np.array, x: np.array, y: np.array, state: np.array
     Returns:
         np.array: x \partial_(\Theta_{ij}) Q y for i, j = 1, ..., n+1
     """
-    n = log_theta.shape[0]
+    n = log_theta.shape[0] - 1
 
-    z = np.zeros(shape=(2*n + 1, 2*n + 1))
+    z = np.zeros(shape=(n + 1, n + 1))
 
     for i in range(n):
         z_sync = x * kronvec_sync(log_theta=log_theta,
@@ -30,35 +31,35 @@ def x_partial_Q_y(log_theta: np.array, x: np.array, y: np.array, state: np.array
         z_met = x * kronvec_met(log_theta=log_theta,
                                 p=y, i=i, n=n, state=state)
 
-        z[i, -1] = sum(z_met)
+        z[i, -1] = z_met.sum()
 
         for j in range(n):
             current = state[2*j: 2*j + 2]
 
-            if sum(current) == 0:
+            if current.sum() == 0:
                 if i == j:
-                    z[i, j] = sum(
-                        sum(z_sync),
-                        sum(z_prim),
-                        sum(z_met)
+                    z[i, j] = sum([
+                        z_sync.sum(),
+                        z_prim.sum(),
+                        z_met.sum()]
                     )
 
-            elif sum(current) == 2:
+            elif current.sum() == 2:
                 z_sync = z_sync.reshape((-1, 4), order="C")
                 z_prim = z_prim.reshape((-1, 4), order="C")
                 z_met = z_met.reshape((-1, 4), order="C")
 
-                z[i, j] = sum(
-                    sum(z_sync[:, 3]),
-                    sum(z_prim[:, [1, 3]]),
-                    sum(z_met[:, [2, 3]])
+                z[i, j] = sum([
+                    z_sync[:, 3].sum(),
+                    z_prim[:, [1, 3]].sum(),
+                    z_met[:, [2, 3]].sum()]
                 )
 
                 if i == j:
-                    z[i, j] += sum(
-                        sum(z_sync[:, 0]),
-                        sum(z_prim[:, [0, 2]]),
-                        sum(z_met[:, [0, 1]])
+                    z[i, j] += sum([
+                        z_sync[:, 0].sum(),
+                        z_prim[:, [0, 2]].sum(),
+                        z_met[:, [0, 1]].sum()]
                     )
 
                 z_sync = z_sync.flatten(order="F")
@@ -72,14 +73,14 @@ def x_partial_Q_y(log_theta: np.array, x: np.array, y: np.array, state: np.array
 
                 if i != j:
                     if current[1] == 1:  # met mutated
-                        z[i, j] = sum(z_met[:, 1])
+                        z[i, j] = z_met[:, 1].sum()
                     else:  # prim mutated
-                        z[i, j] = sum(z_prim[:, 1])
+                        z[i, j] = z_prim[:, 1].sum()
                 else:
-                    z[i, j] = sum(
-                        sum(z_sync[:, 0]),
-                        sum(z_prim),
-                        sum(z_met)
+                    z[i, j] = sum([
+                        z_sync[:, 0].sum(),
+                        z_prim.sum(),
+                        z_met.sum()]
                     )
                 z_sync = z_sync.flatten(order="F")
                 z_prim = z_prim.flatten(order="F")
@@ -87,19 +88,19 @@ def x_partial_Q_y(log_theta: np.array, x: np.array, y: np.array, state: np.array
 
     z_seed = x * kronvec_seed(log_theta=log_theta, p=y, n=n, state=state)
 
-    z[-1, -1] = sum(z_seed)
+    z[-1, -1] = z_seed.sum()
 
     for j in range(n):
         current = state[2*j: 2*j + 2]
 
-        if sum(current) == 2:
+        if current.sum() == 2:
             z_seed = z_seed.reshape((-1, 4), order="C")
 
-            z[-1, j] = sum(z_seed[:, 3])
+            z[-1, j] = z_seed[:, 3].sum()
 
             z_seed = z_seed.flatten(order="F")
 
-        elif sum(current) == 1:
+        elif current.sum() == 1:
             z_seed = z_seed.reshape((-1, 2), order="C").flatten(order="F")
 
     return z
@@ -121,7 +122,7 @@ def R_i_inv_vec(log_theta: np.array, x: np.array, lam: float,  state: np.array, 
     """
     n = log_theta.shape[0] - 1
 
-    lidg = 1 / (kron_diag(log_theta=log_theta, n=n, state=state) - lam)
+    lidg = -1 / (kron_diag(log_theta=log_theta, n=n, state=state) - lam)
     y = lidg * x
 
     for _ in range(sum(state) + 1):
@@ -134,7 +135,8 @@ def R_i_inv_vec(log_theta: np.array, x: np.array, lam: float,  state: np.array, 
 def log_gradient(log_theta: np.array, lam1: float, lam2: float, state: np.array) -> np.array:
 
     n_ss = sum(state)
-    p_0 = np.zeros(n_ss)
+    p_0 = np.zeros(2**n_ss)
+    p_0[0] = 1
     lam = (lam1 * lam2 / (lam1 - lam2))
     p_theta = lam * R_i_inv_vec(
         log_theta=log_theta,
@@ -146,15 +148,19 @@ def log_gradient(log_theta: np.array, lam1: float, lam2: float, state: np.array)
         ),
         lam=lam1,
         state=state)
-    minuent = 1 / p_theta * lam
-    subtrahent = minuent.copy()
-    minuent = R_i_inv_vec(log_theta=log_theta, x=minuent,
+    minuend = 1 / p_theta * lam
+    subtrahend = minuend.copy()
+    minuend = R_i_inv_vec(log_theta=log_theta, x=minuend,
                           lam=lam1, state=state, transpose=True)
-    subtrahent = R_i_inv_vec(log_theta=log_theta, x=minuent,
+    subtrahend = R_i_inv_vec(log_theta=log_theta, x=minuend,
                              lam=lam2, state=state, transpose=True)
-    minuent = x_partial_Q_y(log_theta=log_theta, x=minuent, y=R_i_inv_vec(
-        log_theta=log_theta, x=p_0, lam=lam1, state=state, transpose=True))
-    subtrahent = x_partial_Q_y(log_theta=log_theta, x=subtrahent, y=R_i_inv_vec(
-        log_theta=log_theta, x=p_0, lam=lam2, state=state, transpose=True))
+    minuend = x_partial_Q_y(log_theta=log_theta, x=minuend, y=R_i_inv_vec(
+        log_theta=log_theta, x=p_0, lam=lam1, state=state, transpose=True), state=state)
+    subtrahend = x_partial_Q_y(log_theta=log_theta, x=subtrahend, y=R_i_inv_vec(
+        log_theta=log_theta, x=p_0, lam=lam2, state=state, transpose=True), state=state)
+
+    return minuend - subtrahend
+
+
 
     return minuent - subtrahent
