03ee256d41811494a9f2c9d667e5f66cf28ced3d
huy29433 <hu.linda@aol.com>
2023-05-11 15:03:03 +0000

    edited paths and changed time plots

--- analyses/simulation_script.py
+++ analyses/simulation_script.py
@@ -7,11 +7,11 @@ import json
 cancer_type = "paad"
 log_theta_file = "paad_mixed_08_003"
 
-log_theta = pd.read_csv(f"results/{cancer_type}/{log_theta_file}.csv", index_col=0)
+log_theta = pd.read_csv(f"../results/{cancer_type}/{log_theta_file}.csv", index_col=0)
 lam1, lam2 = log_theta.pop("Sampling").iloc[:2]
 events = log_theta.columns
 log_theta = log_theta.to_numpy()
 
 data = np.vstack([simulations.single_traject_timed(log_theta=log_theta, t_obs=np.inf, rng=np.random.default_rng()) for _ in range(1000000)])
 
-np.save(f"simulations/{cancer_type}/{log_theta_file}.npy", data)
+np.save(f"../simulations/{cancer_type}/{log_theta_file}.npy", data)
