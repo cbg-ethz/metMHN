Fixed crossval-issue, sped up gradient calculation

src/jx/likelihood.py
@@ -394,7 +394,7 @@ def _g_coupled(log_theta: jnp.array, theta_prim: jnp.array, state_joint: jnp.arr
     nk = pTh1_marg.at[-1].get()
     
     # Select the states where x = prim and z are compatible with met 
-    poss_states = obs_states(pTh1_joint, state_joint, True)
+    poss_states = obs_states((n_prim + n_met -1), state_joint, True)
     poss_states_inds = jnp.where(poss_states, size=2**(n_met-1))[0]
     # Prim conditional distribution at first sampling, to be used as starting dist for second sampling
     pTh1_cond_obs = pTh1_joint.at[poss_states_inds].get()
@@ -426,41 +426,3 @@ def _g_coupled(log_theta: jnp.array, theta_prim: jnp.array, state_joint: jnp.arr
     
     score =  jnp.log(nk) + jnp.log(pTh2_marg.at[-1].get())
     return score, g_1 + g_2 + g_3 + g_4, dlam1 * lam1
-
-
-def _g_3_lhs(log_theta: jnp.array, pTh1: jnp.array, pTh2: jnp.array, latent_inds: jnp.array, 
-            second_obs: jnp.array, lam2:jnp.array) -> jnp.array:
-    """Calculates  (pD/pTh_2)^T M lam2*(lam2*I-Q)^(-1)
-
-    Args:
-        log_theta (jnp.array): theta matrix with logarithmic entries
-        pTh1 (jnp.array): Joint probability distribution at first sampling
-        pTh2 (jnp.array): Marginal probability distribution at second sampling
-        latent_inds (jnp.array): Vector holding indices of observed states
-        second_obs (jnp.array): Genotype of datapoint observed at second sampling
-        lam2 (jnp.array): Rate \lambda_2 of second sampling
-
-    Returns:
-        jnp.array
-    """
-    q = jnp.zeros_like(pTh2)
-    q = q.at[-1].set(1/pTh2.at[-1].get())
-    q = lam2*mhn.R_inv_vec(log_theta, q, lam2, second_obs, transpose = True)
-    q_big = jnp.zeros_like(pTh1)
-    q_big =  q_big.at[latent_inds].set(q.at[q.shape[0]//2:].get())
-    return q_big
-
-def prob_seeded(log_theta: jnp.array, x_obs: jnp.array, lam1:jnp.array):
-    x_obs = x_obs.at[::2].get()
-    x_obs = x_obs.at[-1].set(1)
-    m = x_obs.sum()
-    p0 = jnp.zeros(2**m)
-    p0 = p0.at[0].set(1.)
-    pTh1 = lam1 * mhn.R_inv_vec(
-        log_theta=log_theta,
-        x=p0,
-        lam=lam1,
-        state=x_obs)
-    pTh1 = pTh1.reshape((-1,2), order="F")
-    pTh1 =  pTh1 @ jnp.array([[1,1], [0,1]])
-    return pTh1.at[-1, 0].get()/pTh1.at[-1,1].get()

src/regularized_optimization.py
@@ -246,13 +246,11 @@ def value_grad(params: np.array, dat_prim_only: jnp.array, dat_coupled: jnp.arra
         tuple[np.array, np.array]: (likelihood, gradient)
     """
     # Unpack parameters
-    n_prim_only =  dat_prim_only.shape[0]
-    n_coupled = dat_coupled.shape[0]
-    n_prim_met = dat_prim_met.shape[0]
-    n_met_only = dat_met_only.shape[0]
-    n_met = n_coupled + n_prim_met + n_met_only
     log_theta = params[0:-2].reshape((n+1, n+1))
-       
+    score_prim = score_met = score_prim_met = score_coupled  = 0.
+    g_prim = g_met = g_prim_met = g_coupled = jnp.zeros((n+1, n+1)) 
+    n_prim_only, n_met_only, n_prim_met, n_coupled = 1, 0, 0, 0
+
     # Penalties and their derivatives
     l1 = L1(log_theta)
     l1_ = L1_(log_theta)
@@ -269,12 +267,22 @@ def value_grad(params: np.array, dat_prim_only: jnp.array, dat_coupled: jnp.arra
 
     log_theta_prim = log_theta.copy()
     log_theta_prim = log_theta_prim.at[:-1,-1].set(0.)
-    score_prim, g_prim = grad_prim_only(log_theta, dat_prim_only, lam1, n)
-    score_coupled, g_coupled = grad_coupled(log_theta, log_theta_prim, dat_coupled, lam1, lam2, n)
-    #score_prim_met, g_prim_met = grad_prim_only(log_theta_prim, dat_prim_met, lam1, n)
-    #score_met, g_met  = grad_met_only(log_theta, dat_met_only, lam1, lam2, n)
+    if dat_prim_only != None:
+        score_prim, g_prim = grad_prim_only(log_theta, dat_prim_only, lam1, n)
+        n_prim_only =  dat_prim_only.shape[0]
+    if dat_coupled != None:
+        score_coupled, g_coupled = grad_coupled(log_theta, log_theta_prim, dat_coupled, lam1, lam2, n)
+        n_coupled = dat_coupled.shape[0]
+    if dat_prim_met != None:
+        score_prim_met, g_prim_met = grad_prim_only(log_theta_prim, dat_prim_met, lam1, n)
+        n_prim_met = dat_prim_met.shape[0]
+    if dat_met_only != None:
+        score_met, g_met  = grad_met_only(log_theta, dat_met_only, lam1, lam2, n)
+        n_met_only = dat_met_only.shape[0]
+
+    n_met = n_coupled + n_prim_met + n_met_only
     score = (1 - perc_met) * score_prim/n_prim_only + perc_met *\
-        (score_coupled/n_coupled) #+ score_prim_met + score_met)/n_met
+        (score_coupled+ score_prim_met + score_met)/n_met
     g = (1 - perc_met) * g_prim/n_prim_only + perc_met *\
-        (g_coupled/n_coupled) #+ g_prim_met + g_met)/n_met
+        (g_coupled + g_prim_met + g_met)/n_met
     return np.array(-score + penal1 * l1 + penal2 * l2), np.array(-g + penal1 * l1_ + penal2 * l2_)