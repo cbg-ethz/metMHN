4b404402d5f6431df675f1c098377dbfd45cb03b
huy29433 <yanren-linda.hu@klinik.uni-regensburg.de>
2024-06-03 15:34:27 +0200

    started with likelihood testing

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -1,7 +1,9 @@
 import metmhn.Utilityfunctions as utils
 from metmhn.model import MetMHN
+from metmhn.state import State
 import numpy as np
 import unittest
+
 
 class LikelihoodTestCase(unittest.TestCase):
     @classmethod
@@ -13,7 +15,7 @@ class LikelihoodTestCase(unittest.TestCase):
         self.metMHN = MetMHN(log_theta, obs1, obs2)
 
     def test_unreachable(self):
-        """Test that there is an error thrown for an unreachable state""" 
+        """Test that there is an error thrown for an unreachable state"""
         unreachable_state = np.array([0, 1, 1, 1, 0, 0, 0, 1, 0])
         test_cases = [
             ("isPaired", "PT"),
@@ -28,9 +30,9 @@ class LikelihoodTestCase(unittest.TestCase):
                         unreachable_state,
                         met_status=met_status,
                         first_obs=first_obs,)
-                    
+
     def test_invalid_states(self):
-        """Test that there is an error thrown for invalid states""" 
+        """Test that there is an error thrown for invalid states"""
         test_cases = [
             # Metastasis but PT events present
             ("isMetastasis", np.array([0, 1, 1, 1, 0, 0, 0, 1, 1])),
@@ -51,6 +53,33 @@ class LikelihoodTestCase(unittest.TestCase):
                     self.metMHN.likeliest_order(
                         invalid_state,
                         met_status=met_status,)
-                    
+
+    @unittest.skip("Not implemented yet")
+    def test_likelihood(self):
+        """Test that the likelihoods are calculated correctly"""
+        unpaired_diag = self.metMHN._get_diag_unpaired(state=State([1] * 5))
+        test_cases = [
+            # Test with PT events
+            ((0, 3, 2), "absent", None,
+             np.exp(self.log_theta[0, 0]) \
+             / (1 - unpaired_diag[0]) \
+             * np.exp(self.log_theta[3, [0, 3]].sum()) \
+             / (np.exp(self.obs1[0]) - unpaired_diag[2 ** 0]) \
+             * np.exp(self.log_theta[2, [0, 3, 2]].sum()) \
+             / (np.exp(self.obs1[[0, 3]].sum()) - unpaired_diag[2 ** 0 + 2 ** 3]) \
+             * np.exp(self.obs1[[0, 2, 3]].sum()) \
+             / (np.exp(self.obs1[[0, 2, 3]].sum()) - unpaired_diag[2 ** 0 + 2 ** 2 + 2 ** 3]),
+             )]
+
+        for order, met_status, first_obs, expected_likelihood in test_cases:
+            with self.subTest(order=order, met_status=met_status, first_obs=first_obs):
+                likelihood = self.metMHN.likelihood(
+                    order,
+                    met_status=met_status,
+                    first_obs=first_obs,)
+                self.assertAlmostEqual(
+                    likelihood, expected_likelihood, places=5)
+
+
 if __name__ == "__main__":
     unittest.main()
