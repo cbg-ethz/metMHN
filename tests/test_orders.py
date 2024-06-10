3f58030fa7ab7688731fbeeac07152832de41f58
huy29433 <yanren-linda.hu@klinik.uni-regensburg.de>
2024-06-10 18:14:18 +0200

    finished tests

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -3,6 +3,7 @@ from metmhn.model import MetMHN
 from metmhn.state import State, MetState
 import numpy as np
 import unittest
+from numpy.testing import assert_approx_equal as np_assert_approx_equal
 
 
 class LikelihoodTestCase(unittest.TestCase):
@@ -136,53 +137,80 @@ class LikelihoodTestCase(unittest.TestCase):
         unpaired_diag = self.metMHN._get_diag_unpaired(
             state=State.from_seq([1] * 5), seeding=False)
         paired_diag = self.metMHN._get_diag_paired(
-            state=MetState.from_seq([1] * 11))
+            state=MetState.from_seq([1] * 9))
         test_cases = [
             ((0, 1, seeding, 4), (3, 5), self.metMHN._likelihood_pt_mt_timed,
+             # joint 0
              np.exp(self.log_theta[0, 0])
              / (1 - paired_diag[0])
+
+             # seeding
              * np.exp(self.log_theta[self.n, [0, self.n]].sum())
              / (np.exp(self.obs1[0]) - paired_diag[2 ** 0 + 2 ** 1])
+
+             # pt 2
              * np.exp(self.log_theta[2, [0, 2]].sum())
-             / (np.exp(self.obs1[[0]].sum())
+             / (np.exp(self.obs1[[0, self.n]].sum())
                 + np.exp(self.obs2[[0, self.n]].sum())
-                - paired_diag[2 ** 0 + 1 ** 3 + 2 ** seeding])
-             * np.exp(self.obs1[[0, 2]].sum())
-             / (np.exp(self.obs1[[0, 2]].sum())
+                - paired_diag[2 ** 0 + 2 ** 1 + 2 ** seeding])
+
+             # obs1
+             * np.exp(self.obs1[[0, 2, self.n]].sum())
+             / (np.exp(self.obs1[[0, 2, self.n]].sum())
                 + np.exp(self.obs2[[0, self.n]].sum())
                 - paired_diag[2 ** 0 + 2 ** 1 + 2 ** 4 + 2 ** seeding])
+
+             # met 1
              * np.exp(self.log_theta[1, [0, 1, self.n]].sum())
              / (np.exp(self.obs2[[0, self.n]].sum())
                 - unpaired_diag_seeding[2 ** 0 + 2 ** self.n])
+
+             # met 2
              * np.exp(self.log_theta[2, [0, 1, 2, self.n]].sum())
              / (np.exp(self.obs2[[0, 1, self.n]].sum())
                 - unpaired_diag_seeding[2 ** 0 + 2 ** 1 + 2 ** self.n])
+
+             # obs2
              * np.exp(self.obs2[[0, 1, 2, self.n]].sum())
              / (np.exp(self.obs2[[0, 1, 2, self.n]].sum())
                 - unpaired_diag_seeding[
                     2 ** 0 + 2 ** 1 + 2 ** 2 + 2 ** self.n]),
              ),
             ((0, 1, seeding, 5), (6, 2), self.metMHN._likelihood_mt_pt_timed,
+
+             # joint 0
              np.exp(self.log_theta[0, 0])
              / (1 - paired_diag[0])
+
+             # seeding
              * np.exp(self.log_theta[self.n, [0, self.n]].sum())
              / (np.exp(self.obs1[0]) - paired_diag[2 ** 0 + 2 ** 1])
+
+             # met 2
              * np.exp(self.log_theta[2, [0, 2, self.n]].sum())
-             / (np.exp(self.obs1[[0]].sum())
+             / (np.exp(self.obs1[[0, self.n]].sum())
                 + np.exp(self.obs2[[0, self.n]].sum())
-                - paired_diag[2 ** 0 + 1 ** 3 + 2 ** seeding])
+                - paired_diag[2 ** 0 + 2 ** 1 + 2 ** seeding])
+
+             # obs2
              * np.exp(self.obs2[[0, 2, self.n]].sum())
-             / (np.exp(self.obs1[[0]].sum())
+             / (np.exp(self.obs1[[0, self.n]].sum())
                 + np.exp(self.obs2[[0, 2, self.n]].sum())
                 - paired_diag[2 ** 0 + 2 ** 1 + 2 ** 5 + 2 ** seeding])
+
+             # pt 3
              * np.exp(self.log_theta[3, [0, 3]].sum())
-             / (np.exp(self.obs1[[0]].sum()) - unpaired_diag[2 ** 0])
+             / (np.exp(self.obs1[[0, self.n]].sum()) - unpaired_diag[2 ** 0])
+
+             # pt 1
              * np.exp(self.log_theta[1, [0, 1, 3]].sum())
-             / (np.exp(self.obs2[[0, 3]].sum())
+             / (np.exp(self.obs1[[0, 3, self.n]].sum())
                 - unpaired_diag[2 ** 0 + 2 ** 3])
-             * np.exp(self.obs2[[0, 1, 3]].sum())
-             / (np.exp(self.obs2[[0, 1, 3]].sum())
-                - unpaired_diag_seeding[2 ** 0 + 2 ** 1 + 2 ** 3]),
+
+             # obs1
+             * np.exp(self.obs1[[0, 1, 3, self.n]].sum())
+             / (np.exp(self.obs1[[0, 1, 3, self.n]].sum())
+                - unpaired_diag[2 ** 0 + 2 ** 1 + 2 ** 3]),
              ),
         ]
         for order1, order2, func, expected_likelihood in test_cases:
@@ -191,17 +219,42 @@ class LikelihoodTestCase(unittest.TestCase):
                     order1,
                     order2,)
                 np_assert_approx_equal(
-                    likelihood, expected_likelihood, places=5)
-                
+                    likelihood, expected_likelihood)
+
     def test_paired_likelihood(self):
         """Test that the likelihoods for paired orders are calculated correctly"""
-        
+
         seeding = self.n * 2
+        state = MetState([0, 1, 3, 5, 6, seeding], size=2 * self.n + 1)
         test_cases = [
-            MetState([0, 1, 3, 5, 6])
+            "PT",
+            "Met",
+            "unknown",
+            "sync"
         ]
-
-
+        for first_obs in test_cases:
+            with self.subTest(first_obs=first_obs):
+                if first_obs == "sync":
+                    with self.assertWarns(Warning):
+                        order, likelihood = self.metMHN.likeliest_order(
+                            state=state,
+                            met_status="isPaired",
+                            first_obs=first_obs,)
+                        np_assert_approx_equal(
+                            likelihood,
+                            self.metMHN.likelihood(
+                                order, met_status="isPaired", first_obs=first_obs),
+                        )
+                else:
+                    order, likelihood = self.metMHN.likeliest_order(
+                        state=state,
+                        met_status="isPaired",
+                        first_obs=first_obs,)
+                    np_assert_approx_equal(
+                        likelihood,
+                        self.metMHN.likelihood(
+                            order, met_status="isPaired", first_obs=first_obs),
+                    )
 
 
 if __name__ == "__main__":
